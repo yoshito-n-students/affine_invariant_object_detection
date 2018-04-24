836e77aad706aa85689b016a3813eb17e6f1256c
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2018-04-25 07:47:27 +0900

    skeleton of label drawer classes

---
+++ include/label_detection/label_drawer.hpp
@@ -0,0 +1,10 @@
+#ifndef LABEL_DETECTION_LABEL_DRAWER_HPP
+#define LABEL_DETECTION_LABEL_DRAWER_HPP
+
+namespace label_detection {
+
+class LabelDrawer {};
+
+} // namespace label_detection
+
+#endif
---
+++ include/label_detection/label_drawing_node.hpp
@@ -0,0 +1,15 @@
+#ifndef LABEL_DETECTION_LABEL_DRAWING_NODE_HPP
+#define LABEL_DETECTION_LABEL_DRAWING_NODE_HPP
+
+#include <label_detection/label_drawer.hpp>
+
+namespace label_detection {
+
+class LabelDrawingNode {
+private:
+  LabelDrawer drawer_;
+};
+
+} // namespace label_detection
+
+#endif
